c52c3dae969448ec744cd0d264d4670a1f978ce2
rekarnar <rekarnar@gmail.com>
2014-04-02 09:26:33 +1300

    updating entities

--- module/templates/entities/_entity.js
+++ module/templates/entities/_entity.js
@@ -1,5 +1,5 @@
-define(['app'], function(App){
-  App.module('Entities', function(Entities, App, Backbone, Marionette, $, _){
+define(['app'], function(App) {
+  App.module('Entities', function(Entities, App, Backbone, Marionette, $, _) {
     var contextName = 'Entity';
     Entities.<%= cname %> = Backbone.Model.extend({
       urlRoot: '<%= name %>',
@@ -11,29 +11,27 @@ define(['app'], function(App){
 
       validate: function(attrs, options) {
         var errors = {}
-    //     if (! attrs.firstName) {
-    //       errors.firstName = 'can't be blank';
-    //     }
-    //     if (! attrs.lastName) {
-    //       errors.lastName = 'can't be blank';
+    //     if (! attrs.something) {
+    //       errors.something = 'can't be blank';
     //     }
     //     else{
-    //       if (attrs.lastName.length < 2) {
-    //         errors.lastName = 'is too short';
+    //       if (attrs.something.length < 2) {
+    //         errors.something = 'is too short';
     //       }
     //     }
-    //     if( ! _.isEmpty(errors)){
-    //       return errors;
-    //     }
+        if( ! _.isEmpty(errors)) {
+          return errors;
+        }
       }
     });
 
     Entities.<%= cname %>Collection = Backbone.Collection.extend({
-      url: '<%= name %>'
+      url: '<%= name %>',
+      model: Entities.<%= cname %>
     });
 
-    var initialize<%= cname %>s = function(){
-      App.log('<%= cname %>s init', contextName, 1);
+    var initialize<%= cname %>s = function() {
+      App.log('Initializing Fake <%= cname %>s', contextName, 1);
       var <%= cname %> = Backbone.Model.extend({});
 
       var fake<%= cname %>s = [
@@ -45,29 +43,29 @@ define(['app'], function(App){
     };
 
     var API = {
-      get<%= cname %>Entities: function(){
-        App.log('get<%= cname %>s called', contextName, 1);
-        var <%= name %>s = new Entities.<%= cname %>Collection();
+      get<%= cname %>Entities: function() {
+        App.log('<%= name %>:entities event detected', contextName, 1);
+        var <%= name %>Collection = new Entities.<%= cname %>Collection();
         var defer = $.Deferred();
-        <%= name %>s.fetch({
+        <%= name %>Collection.fetch({
           complete: function(data){
-            App.log('fake datas', contextName, 1);
-            defer.resolve(<%= name %>s);
+            defer.resolve(<%= name %>Collection); // send back the collection
           },
-          success: function(data){
-            App.log('success data', contextName, 1);
-            defer.resolve(data);
-          }
+          // success: function(data){
+            // App.log('success data', contextName, 1);
+            // defer.resolve(data);
+          // }
         });
+        // chain the above promise,
         var promise = defer.promise();
-        $.when(promise).done(function(<%= name %>s){
-          App.log('promise running: ' + <%= name %>s.length, contextName, 1);
-          if (<%= name %>s.length === 0) {
-            // if we don't have any <%= name %>s yet, create some for convenience
-            var models = initialize<%= cname %>s();
+        $.when(promise).done(function(<%= name %>Collection) {
+          // check to see if it had content:
+          if (<%= name %>Collection.length === 0) { // if not, get defaults.
+            // FAKE NETWORK LAG
             setTimeout(function () {
-              App.trigger('page:register', models); // add each <%= name %> to the menu
-              <%= name %>s.reset(models);
+              // App.trigger('page:register', models); // add each <%= name %> to the menu
+              // if we don't have any imageCollection yet, create some for convenience
+              <%= name %>Collection.reset(initialize<%= cname %>s().models); // update the collection
             }, 2000);
 
           }
@@ -77,17 +75,18 @@ define(['app'], function(App){
 
     };
 
-    App.reqres.setHandler('<%= name %>:entities', function(){
+    App.reqres.setHandler('<%= name %>:entities', function() {
       return API.get<%= cname %>Entities();
     });
 
-    // App.reqres.setHandler('<%= name %>:entity', function(id){
-      // return API.getContactEntity(id);
+    // App.reqres.setHandler('<%= name %>:entity', function(id) {
+      // return API.get<%= cname %>Entity(id);
     // });
 
-    // App.reqres.setHandler('contact:entity:new', function(id){
-      // return new Entities.Contact();
-    // });
+    App.reqres.setHandler('<%= name %>:entity:new', function(id) {
+      App.log('Making new <%= name %>', this.name, 1);
+      return new Entities.<%= cname %>();
+    });
   });
 
   return ;
